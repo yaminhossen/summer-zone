ff119c2da7e6e18f3d0ed4578c4146fd6d88847c
yaminhossen <streatdot616@gmail.com>
2023-02-24 08:59:47 +0600

    expert details page rearange

--- src/components/Pages/Home/ExpertDetails.js
+++ src/components/Pages/Home/ExpertDetails.js
@@ -5,7 +5,7 @@ import jacket3 from '../../../images/explore/sneakars (4).png'
 
 const ExpertDetails = () => {
     return (
-        <div className='mx-28 my-14'>
+        <div className='mx-14 lg:mx-28 my-14'>
             <div className='flex'>
                 <div className='lg:w-3/5 lg:mr-8 '>
                     <div className=' lg:h-96 flex justify-center items-center bg-yellow-100 rounded-xl  overflow-hidden'>
@@ -25,8 +25,8 @@ const ExpertDetails = () => {
                 </div>
 
             </div>
-            <div className='flex'>
-                <div className='w-4/6'>
+            <div className='lg:flex'>
+                <div className='lg:w-4/6'>
                     <h3 className='font-bold text-2xl my-8'>Sustainable Export Shoe</h3>
                     <p className='flex'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 pt-1 h-5 text-yellow-500">
@@ -90,7 +90,7 @@ const ExpertDetails = () => {
                         </div>
                     </div>
                 </div>
-                <div className='w-2/6  m-4 '>
+                <div className='lg:w-2/6  lg:m-4 '>
                     <div className=' mt-8 shadow-lg border-t-8 shadow-slate-300 p-4 rounded-lg h-full '>
 
                         <div className='flex justify-between'>
@@ -102,20 +102,68 @@ const ExpertDetails = () => {
 
                             </p>
                         </div>
-                        <div className='my-5'>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia!</h3>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia!</h3>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia!</h3>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia!</h3>
+                        <div>
+                            <div className='mt-4 '>
+                                <h2 className='mb-2 font-bold '>Quantity: 3</h2>
+                                <div className='flex'>
+                                    <div className='mr-4 cursor-pointer ring-2 ring-slate-400 rounded-full p-'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
+                                    </svg>
+
+                                    </div>
+                                    <div className='mr-4 p- font-bold'>3</div>
+                                    <div className='cursor-pointer ring-2 ring-slate-400 rounded-full p-'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <h2 className='my-4 font-bold'>Size: lg</h2>
+                                    <div className='flex'>
+                                        <p className='ring-2 hover:ring-0  ring-slate-400 py-1 mr-2 px-6 rounded font-bold cursor-pointer border-none  hover:bg-blue-500 hover:text-white'>s</p>
+                                        <p className=' hover:ring-0  ring-slate-400 ring-2 py-1 mr-2 px-6 rounded font-bold cursor-pointer hover:bg-blue-500 hover:text-white'>md</p>
+                                        <p className=' hover:ring-0  ring-slate-400 ring-2 py-1 mr-2 px-6 rounded font-bold cursor-pointer hover:bg-blue-500 hover:text-white'>lg</p>
+                                        <p className=' hover:ring-0  ring-slate-400 ring-2 py-1 mr-2 px-6 rounded font-bold cursor-pointer hover:bg-blue-500 hover:text-white'>xl</p>
+                                        <p className=' hover:ring-0  ring-slate-400 ring-2 py-1 mr-2 px-6 rounded font-bold cursor-pointer hover:bg-blue-500 hover:text-white'>xxl</p>
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mt-8'>
+                            <div className='flex justify-between my-4'>
+                                <div className='text-md font-bold text-slate-500'>$122 x 3</div>
+                                <div className='font-bold'>$366.00</div>
+                            </div>
+                            <div className="divider my-0 "></div>
+                            <div className='flex justify-between my-4'>
+                                <div className='text-md font-bold text-slate-500'>Shipping estimate</div>
+                                <div className='font-bold'>$8.00</div>
+                            </div>
+                            <div className="divider my-0 "></div>
+                            <div className='flex justify-between my-4'>
+                                <div className='text-md font-bold text-slate-500'>Tax estimate</div>
+                                <div className='font-bold'>$20.00</div>
+                            </div>
+                            <div className="divider my-0 "></div>
+                            <div className='flex justify-between'>
+                                <div className='font-bold text-xl'>Total</div>
+                                <div className='font-bold'>$394.00</div>
+                            </div>
+
                         </div>
                         <div className='flex justify-between mt-10 mb-4'>
                             <button className='p-3 font-bold text-center w-full border-2 mt- rounded-xl bg-slate-500 text-white hover:bg-slate-600'>Buy Now</button>
                             <button className='p-3 font-bold text-center w-full border-2  rounded-xl bg-slate-500 text-white hover:bg-slate-600'>Add to Cart</button>
                         </div>
-                        <div className='flex justify-between'>
+                        {/* <div className='flex justify-between'>
                             <h3 className='font-bold text-lg'>Total:</h3>
                             <p className='font-bold text-lg'>$122.00</p>
-                        </div>
+                        </div> */}
                     </div>
 
                 </div>

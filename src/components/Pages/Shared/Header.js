e3052e9d254a0ce0d4c8c43cbc5ba12684085e70
yaminhossen <streatdot616@gmail.com>
2023-02-24 09:27:16 +0600

    navbar style added

--- src/components/Pages/Shared/Header.js
+++ src/components/Pages/Shared/Header.js
@@ -52,7 +52,12 @@ const Header = () => {
                 <div className="navbar-end lg:mr-24">
                     {
                         user ?
-                            <button className='pr-8 font-semibold text-blue-500' >login by {user?.email}</button>
+                            <button className='hidden lg:block md:block px-2 mr-4 rounded-full font-serif text-blue-500 border-blue-400 border-2' ><span className='flex justify-center items-center'>
+                                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-1">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                                </span> <span>{user?.email}</span>
+                            </span></button>
                             :
                             <></>
                     }
